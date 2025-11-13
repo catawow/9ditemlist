23712215b20872b3d199b9f5043b1ea8931d540a
catawow <151408747+catawow@users.noreply.github.com>
2025-11-13 21:57:56 +0100

    data.js

---
+++ data.js
@@ -0,0 +1,84 @@
+const DATA={
+  "Sales Pocket (100 million) 1":{
+    image: "Poze/image/Sales Pocket + (100 million) 1.png",
+    detailImage: "Poze/detalimage/Sales Pocket + (100 million) 1.png",
+    variants:[
+      {source:{tab:'Resource',map:'Hefei',npc:'Hefei StorageGekeeper Guo Dasham'},
+      success:100,
+      ingredients:{"Sales Pocket (10 million) 1":10}},
+    ]
+  },
+
+  "Sales Pocket (10 million) 1":{
+    image: "Poze/image/Sales Pocket + (10 million) 1.png",
+    detailImage: "Poze/detalimage/Sales Pocket + (10 million) 1.png",
+    variants:[
+      {source:{tab:'Trade',map:'Hefei',npc:'Hefei StorageGekeeper Guo Dasham'},
+      success:100,
+      ingredients:{"10,000,100 Gold":1}},
+    ]
+  },
+
+  "10,000,100 Gold":{
+    image: "Poze/image/gold.png",
+  },
+
+  "Sales Pocket (1000 million)":{
+    image: "Poze/image/Sales Pocket (1000 million).png",
+    detailImage: "Poze/detalimage/Sales Pocket (1000 million).png",
+    variants:[
+      {source:{tab:'Resource',map:'Hefei',npc:'Hefei StorageGekeeper Guo Dasham'},
+      success:100,
+      ingredients:{"Sales Pocket (100 million) 1":10}},
+    ]
+  },
+
+  "Sales Pocket (100 million) 10":{
+    image: "Poze/image/Sales Pocket + (100 million) 1.png",
+    detailImage: "Poze/detalimage/Sales Pocket + (100 million) 1.png",
+    variants:[
+      {source:{tab:'Resource',map:'Hefei',npc:'Hefei StorageGekeeper Guo Dasham'},
+      success:100,
+      ingredients:{"Sales Pocket (1000 million)":1}},
+    ]
+  },
+
+ "Sales Pocket (10 million) 10":{
+    image: "Poze/image/Sales Pocket + (10 million) 1.png",
+    detailImage: "Poze/detalimage/Sales Pocket + (10 million) 1.png",
+    variants:[
+      {source:{tab:'Resource',map:'Hefei',npc:'Hefei StorageGekeeper Guo Dasham'},
+      success:100,
+      ingredients:{"Sales Pocket (100 million) 1":1}},
+    ]
+  },
+
+ "Infinite Vaisravana Costume (F) 1":{
+    image: "Poze/image/Infinite Vaisravana Costume (F) 1.png",
+    detailImage: "Poze/detalimage/Infinite Vaisravana Costume (F) 1.png",
+    variants:[
+      {source:{tab:'Resource',map:'Loulan',npc:'Infinite Helper Lei Woodo'},
+      success:100,
+      ingredients:{"Infinite Dark Soul Costume (F) 1":1, "Celestial Yellow Dragon of Darkness Clothes(F)":1, "United Spirits Bead":2, "Ultimate Tablet":3, "Wave Healing Stone":10, "Infinite Spirit (High)":100}},
+    ]
+  },
+
+ "Infinite Dark Soul Costume (F) 1":{
+    image: "Poze/image/Infinite Dark Soul Costume (F) 1.png",
+    detailImage: "Poze/detalimage/Infinite Dark Soul Costume (F) 1.png",
+    variants:[
+      {source:{tab:'Resource',map:'Loulan',npc:'Infinite Helper Lei Woodo'},
+      success:100,
+      ingredients:{"Dark Soul Costume Exchange Ticket":1}},
+      {source:{tab:'Create',map:'Loulan',npc:'Infinite Master Man Sangja'},
+      success:5,
+      ingredients:{"12":1}},
+    ]
+  },
+
+  "Dark Soul Costume Exchange Ticket":{
+    image: "Poze/image/Dark Soul Costume Exchange Ticket.png",
+    detailImage: "Poze/detalimage/Dark Soul Costume Exchange Ticket.png",
+  },
+
+};
